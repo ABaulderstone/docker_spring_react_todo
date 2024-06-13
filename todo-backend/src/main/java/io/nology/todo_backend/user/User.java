aa4bfc31ad17e5805ac355eb276a20cea98d6241
ABaulderstone <alex.baulderstone@gmail.com>
2024-06-13 16:27:31 +1000

    adds user entity

---
+++ todo-backend/src/main/java/io/nology/todo_backend/user/User.java
@@ -0,0 +1,18 @@
+package io.nology.todo_backend.user;
+
+import io.nology.todo_backend.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+}
