694910abac795bbc604f7f29cec97d07a7d0c6cf
ABaulderstone <alex.baulderstone@gmail.com>
2024-06-14 14:55:47 +1000

    updates user entity to implement user details, adds jwt dependencies

--- todo-backend/src/main/java/io/nology/todo_backend/user/User.java
+++ todo-backend/src/main/java/io/nology/todo_backend/user/User.java
@@ -1,5 +1,11 @@
 package io.nology.todo_backend.user;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import io.nology.todo_backend.common.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -7,12 +13,49 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails {
 
     @Column(unique = true, nullable = false)
     private String email;
 
     @Column(nullable = false)
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        // getUsername is the default coming from UserDetails but we want email as
+        // unique identifier
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
