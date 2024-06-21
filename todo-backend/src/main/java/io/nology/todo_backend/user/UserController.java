b0b454b1ce8efe83203e4ecffa48d11be6a490d2
ABaulderstone <alex.baulderstone@gmail.com>
2024-06-21 11:59:08 +0930

    current user endpoint shows current user

--- todo-backend/src/main/java/io/nology/todo_backend/user/UserController.java
+++ todo-backend/src/main/java/io/nology/todo_backend/user/UserController.java
@@ -1,6 +1,8 @@
 package io.nology.todo_backend.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,10 +21,12 @@ public class UserController {
     }
 
     @GetMapping("/current")
-    public String currentUser() {
+    public ResponseEntity<User> currentUser() throws NumberFormatException, Exception {
         Authentication authenticationObj = SecurityContextHolder.getContext().getAuthentication();
         String currentId = (String) authenticationObj.getPrincipal();
-        return String.format("The current user id is %s", currentId);
+        User currentUser = this.userService.loadById(Long.parseLong(currentId))
+                .orElseThrow(() -> new Exception("No user"));
+        return new ResponseEntity<User>(currentUser, HttpStatus.OK);
     }
 
 }
