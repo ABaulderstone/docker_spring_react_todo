79dfc552a4ba7dfd2295fcd3705b4decbdaf3a96
ABaulderstone <alex.baulderstone@gmail.com>
2024-06-28 10:47:14 +1000

    adds auth tests

--- todo-backend/src/test/java/io/nology/todo_backend/auth/AuthEndToEnd.java
+++ todo-backend/src/test/java/io/nology/todo_backend/auth/AuthEndToEnd.java
@@ -17,7 +17,7 @@ public class AuthEndToEnd extends EndToEndTest {
     }
 
     @Test
-    public void existingUserCanLogin() {
+    public void existingUserCanLoginWithCorrectPassword() {
         User plainUser = getDataLoader().getUser("plainUser");
         String password = getDataLoader().getRawPassword("plainUser");
         LoginDTO body = new LoginDTO();
@@ -25,7 +25,59 @@ public class AuthEndToEnd extends EndToEndTest {
         body.setPassword(password);
 
         givenNoAuthHeader().contentType(ContentType.JSON).body(body).post("/auth/login").then().statusCode(200);
+    }
 
+    @Test
+    public void existingUserCanNotLoginWithIncorrectPassword() {
+        User plainUser = getDataLoader().getUser("plainUser");
+        LoginDTO body = new LoginDTO();
+        body.setEmail(plainUser.getEmail());
+        body.setPassword("banana");
+        givenNoAuthHeader().contentType(ContentType.JSON).body(body).post("/auth/login").then().statusCode(401);
+    }
+
+    @Test
+    public void novelUserCanNotLogin() {
+        LoginDTO body = new LoginDTO();
+        body.setEmail("someguy@fake.com");
+        body.setPassword("banana");
+        givenNoAuthHeader().contentType(ContentType.JSON).body(body).post("/auth/login").then().statusCode(401);
+    }
+
+    @Test
+    public void newUserCanRegisterWithCorrectDetails() {
+        RegisterDTO body = new RegisterDTO();
+        body.setEmail("new-user@test.com");
+        body.setPassword("Password2!");
+        body.setPasswordConfirm("Password2!");
+        givenNoAuthHeader().contentType(ContentType.JSON).body(body).post("/auth/register").then().statusCode(200);
+    }
+
+    @Test
+    public void newUserCanNotRegisterWithInsufficentPassword() {
+        RegisterDTO body = new RegisterDTO();
+        body.setEmail("new-user@test.com");
+        body.setPassword("banana");
+        body.setPasswordConfirm("banana");
+        givenNoAuthHeader().contentType(ContentType.JSON).body(body).post("/auth/register").then().statusCode(400);
+    }
+
+    @Test
+    public void newUserCanNotRegisterWithoutMatchingPasswordConfirm() {
+        RegisterDTO body = new RegisterDTO();
+        body.setEmail("new-user@test.com");
+        body.setPassword("Password2!");
+        body.setPasswordConfirm("banana");
+        givenNoAuthHeader().contentType(ContentType.JSON).body(body).post("/auth/register").then().statusCode(400);
+    }
+
+    @Test
+    public void newUserCanNotRegisterWithInvalidEmail() {
+        RegisterDTO body = new RegisterDTO();
+        body.setEmail("new-user");
+        body.setPassword("Password2!");
+        body.setPasswordConfirm("Password2!");
+        givenNoAuthHeader().contentType(ContentType.JSON).body(body).post("/auth/register").then().statusCode(400);
     }
 
 }
