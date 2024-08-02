7d8f61248f6120d22e8ae410baa9a5a4ec1cf441
ABaulderstone <alex.baulderstone@gmail.com>
2024-08-02 15:03:56 +1000

    category service layer unit tests added

--- todo-backend/src/test/java/io/nology/todo_backend/category/CategoryServiceUnitTest.java
+++ todo-backend/src/test/java/io/nology/todo_backend/category/CategoryServiceUnitTest.java
@@ -2,8 +2,10 @@ package io.nology.todo_backend.category;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -54,14 +56,58 @@ public class CategoryServiceUnitTest {
 
         when(mapper.map(categoryDTO, Category.class)).thenReturn(category);
         when(userService.loadById(mockUserId)).thenReturn(Optional.of(mockUser));
-        when(categoryRepository.save(any(Category.class))).thenReturn(category);
         doReturn(mockUserId).when(categoryService).getCurrentUserId();
+        when(categoryRepository.save(any(Category.class))).thenReturn(category);
 
         Category result = categoryService.create(categoryDTO);
         assertNotNull(result);
         assertEquals(category, result);
         verify(categoryRepository).save(category);
 
+    }
+
+    @Test
+    void create_maxCategories_failure() throws Exception {
+        CreateCategoryDTO createCategoryDTO = new CreateCategoryDTO();
+        Category category = new Category();
+        User mockUser = new User();
+        Long mockUserId = 1L;
+        mockUser.setId(mockUserId);
+        ArrayList<Category> userCategories = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Category cat = new Category();
+            cat.setColor("#fffff" + i);
+            cat.setName("Cat " + i);
+            userCategories.add(cat);
+        }
+        mockUser.setCategories(userCategories);
+        when(mapper.map(createCategoryDTO, Category.class)).thenReturn(category);
+        doReturn(mockUserId).when(categoryService).getCurrentUserId();
+        when(userService.loadById(mockUserId)).thenReturn(Optional.of(mockUser));
+
+        assertThrows(Exception.class, () -> categoryService.create(createCategoryDTO));
+        verify(categoryRepository, never()).save(any());
+    }
+
+    @Test
+    void findCurrentUserCategories_success() {
+        User mockUser = new User();
+        Long mockUserId = 1L;
+        mockUser.setId(mockUserId);
+        ArrayList<Category> userCategories = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Category cat = new Category();
+            cat.setColor("#fffff" + i);
+            cat.setName("Cat " + i);
+            userCategories.add(cat);
+        }
+        mockUser.setCategories(userCategories);
+        doReturn(mockUserId).when(categoryService).getCurrentUserId();
+        when(categoryRepository.findbyUserId(mockUserId)).thenReturn(userCategories);
+
+        List<Category> result = categoryService.findCurrentUserCategories();
+        assertEquals(userCategories, result);
+        verify(categoryRepository).findbyUserId(mockUserId);
     }
 
 }
