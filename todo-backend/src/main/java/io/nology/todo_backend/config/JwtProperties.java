9504d434f41440b013994bd14372dd36c8b141f7
ABaulderstone <alex.baulderstone@gmail.com>
2024-09-09 12:28:19 +0930

    updates jwt config

--- todo-backend/src/main/java/io/nology/todo_backend/config/JwtProperties.java
+++ todo-backend/src/main/java/io/nology/todo_backend/config/JwtProperties.java
@@ -1,23 +1,14 @@
 package io.nology.todo_backend.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
-import jakarta.annotation.PostConstruct;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Configuration
+@ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
-    @Autowired
-    private Environment env;
     private String secretKey;
     private long expirationTime;
-
-    @PostConstruct
-    private void init() {
-        this.secretKey = env.getProperty("JWT_SECRET_KEY");
-        this.expirationTime = Long.parseLong(env.getProperty("JWT_EXPIRY"));
-    }
 
     public String getSecretKey() {
         return secretKey;
